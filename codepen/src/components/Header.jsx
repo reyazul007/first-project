8a803bcab2fc7479051a268c9434b64168f6b909
reyazul007 <reyazulansari296@gmail.com>
2025-02-06 19:59:49 +0545

    login half done

--- codepen/src/components/Header.jsx
+++ codepen/src/components/Header.jsx
@@ -1,31 +1,37 @@
 import { AppBar, Toolbar, styled, Button } from "@mui/material";
 import CloudIcon from '@mui/icons-material/Cloud';
 import SettingsIcon from '@mui/icons-material/Settings';
+import { useState } from "react";
+import Login from "./Login";
 
 const Container = styled(AppBar)`
   background: #060606;
   height: 8vh;
 `;
+
 const Header = () => {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
   const logo =
     "https://blog.codepen.io/wp-content/uploads/2023/09/logo-white.png";
   return (
-    <Container position="static">
-      <Toolbar style={{ justifyContent: "space-between" }}>
-        <img src={logo} alt="logo" style={{ width: "40px" }} />
-        <div>
-          <Button variant="contained" color="primary" style={{  marginRight: "8px",background: "#616a6b", minHeight: "43px" }}>
-            <CloudIcon style={{ marginRight: "4px" }} /> Save
-          </Button>
-          <Button variant="contained" color="primary" style={{ marginRight: "8px", background: "#616a6b", minHeight: "43px"}}>
-            <SettingsIcon style={{ marginRight: "4px" }} /> Settings
-          </Button>
-          <Button variant="contained" color="success"style={{ marginRight: "8px", minHeight: "43px" }}>Sign up</Button>
-          <Button variant="contained" color="primary"style={{ minHeight: "43px" }}>Log in</Button>
-          
-        </div>
-      </Toolbar>
-    </Container>
+    <>
+      <Container position="static">
+        <Toolbar style={{ justifyContent: "space-between" }}>
+          <img src={logo} alt="logo" style={{ width: "40px" }} />
+          <div>
+            <Button variant="contained" color="primary" style={{  marginRight: "8px",background: "#616a6b", minHeight: "43px" }}>
+              <CloudIcon style={{ marginRight: "4px" }} /> Save
+            </Button>
+            <Button variant="contained" color="primary" style={{ marginRight: "8px", background: "#616a6b", minHeight: "43px"}}>
+              <SettingsIcon style={{ marginRight: "4px" }} /> Settings
+            </Button>
+            <Button variant="contained" color="success"style={{ marginRight: "8px", minHeight: "43px" }}>Sign up</Button>
+            <Button variant="contained" color="primary"style={{ minHeight: "43px" }} onClick={() => setIsLoginOpen(true)}>Log in</Button>
+          </div>
+        </Toolbar>
+      </Container>
+      <Login isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
+    </>
   );
 };
 
--- codepen/src/components/Home.jsx
+++ codepen/src/components/Home.jsx
@@ -1,5 +1,3 @@
-
-
 import Header from './Header';
 import Code from './Code';
 import Result from './Result';
