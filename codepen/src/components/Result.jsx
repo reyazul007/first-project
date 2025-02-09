f67a144b161ac343bdb395987226b135372fdf6b
reyazul007 <reyazulansari296@gmail.com>
2025-02-09 20:21:15 +0545

    project Completed

--- codepen/src/components/Result.jsx
+++ codepen/src/components/Result.jsx
@@ -24,7 +24,7 @@ const Result = () => {
 
     const timeout = setTimeout(() => {
       setSrc(srcCode);
-    }, 1000);
+    }, 500);
 
     return () => clearTimeout(timeout);
   }, [html, css, js]);
